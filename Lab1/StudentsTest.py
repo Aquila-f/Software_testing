cbb38d4ff629e6fea929c947be529a1288a60b1d
Aquila <qwertgb1234@gmail.com>
2024-03-19 01:01:56 +0800

    update name

---
+++ Lab1/StudentsTest.py
@@ -0,0 +1,43 @@
+import unittest
+import Students
+
+class Test(unittest.TestCase):
+    students = Students.Students()
+
+    user_name = ['John', 'Mary','Thomas','Jane']
+    user_id = []
+
+    # test case function to check the Students.set_name function
+    def test_0_set_name(self):
+        print("Start set_name test")
+        for i, name in enumerate(self.user_name):
+            id = self.students.set_name(name)
+            self.assertTrue(id >= 0)
+            self.assertEqual(id, i)
+            self.assertNotIn(id, self.user_id)
+
+            self.user_id.append(id)
+            print(f"{id} {name}")
+        
+        print("Finish set_name test")
+        #TODO
+        pass
+
+    # test case function to check the Students.get_name function
+    def test_1_get_name(self):
+        print(f"user_id length =  {len(self.user_id)}")
+        print(f"user_name length =  {len(self.user_name)}")
+        self.assertEqual(len(self.user_id), len(self.user_name))
+
+        for i, id in enumerate(self.user_id):
+            name = self.students.get_name(id)
+            self.assertEqual(name, self.user_name[i])
+            print(f"id {id} : {name}")
+        
+        # additional test
+        name = self.students.get_name(len(self.user_id))
+        self.assertEqual(name, 'There is no such user')
+
+        print("Finish get_name test")
+        #TODO
+        pass
